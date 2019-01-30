5aa3b22e417b38ea7f1350563d2fa33a0478fdfd
oz <oz@localhost.localdomain>
2019-01-30 18:36:15 +0800

    new layout framework

--- src/main/java/org/oz/fx/App3.java
+++ src/main/java/org/oz/fx/App3.java
@@ -1,17 +1,80 @@
 package org.oz.fx;
 
+import com.jfoenix.controls.JFXListView;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
 public class App3 extends Application {
 
 
-    public final static class AppController {
+    public final static class AppController implements Initializable {
+
+
+        @FXML
+        JFXListView list_RFID;
+
+
+        @FXML
+        JFXListView list_monitor;
+
+
+        @Override
+        public void initialize(URL location, ResourceBundle resources) {
+
+            System.out.println("initialize.........");
+
+            initData();
+
+            initView();
+
+        }
+
+
+        private void initView() {
+
+            ObservableList readerItems = list_RFID.getItems();
+
+            ObservableList monitorItems = list_monitor.getItems();
+
+
+            for (int i = 0; i < 30; i++) {
+
+                readerItems.add(new Label("reader" + (i + 1)));
+
+                monitorItems.add(new Label("monitor" + (i + 1)));
+
+            }
+        }
+
+        private void initData() {
+
+            System.out.println("init data 。。。。");
+        }
+
+
+
+        /*        @FXML
+        private void initialize() {
+
+            System.out.println("initialize.........");
+
+            initData();
+
+            initView();
+
+        }*/
 
 
     }
